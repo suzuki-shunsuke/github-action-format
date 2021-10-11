be1317960bf4e812442607e1b0b6e6528de2402a
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2021-10-11 13:32:40 +0900

    feat: implement basic function

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # github-action-format
+
 GitHub Actions to format code and push commit
+
+## Requirements
+
+* [ghcp](https://github.com/int128/ghcp)
+
+## License
+
+[MIT](LICENSE)
---
+++ main.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -eu
+set -o pipefail
+
+tempfile=$(mktemp)
+eval "$INPUT_COMMAND" > "$tempfile"
+if [ 0 -eq "$(wc -l < "$tempfile")" ]; then
+	exit 0
+fi
+
+# shellcheck disable=SC2046
+ghcp commit -r "$GITHUB_REPOSITORY" -b "$GITHUB_HEAD_REF" \
+	-m "$INPUT_COMMIT_MESSAGE" \
+	-C "$ROOT_DIR" $(cat "$tempfile")
+rm "$tempfile"
+exit 1
