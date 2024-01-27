4ec58f71281bde13a8ccb7c1bcce237b8cda9f5c
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2024-01-27 16:40:38 +0900

    fix: replace GIHTUB_REF with GITHUB_REF_NAME

--- main.sh
+++ main.sh
@@ -11,26 +11,23 @@ fi
 
 echo "::error :: Files aren't formatted"
 
-if [ "$SKIP_PUSH" = true ]; then
+if [ "$SKIP_PUSH" = true ] || [[ $GITHUB_REF =~ ^refs/tags/ ]]; then
 	rm "$tempfile"
-	exit 1
-fi
-
-branch=${GITHUB_HEAD_REF:-$GITHUB_REF}
-
-if [[ $branch =~ ^refs/tags/ ]]; then
 	exit 1
 fi
 
 if ! ghcp -v; then
 	echo "::error :: ghcp is required to push a commit. Please install https://github.com/int128/ghcp"
+	rm "$tempfile"
 	exit 1
 fi
 
 echo "::notice :: Pushing a commit using ghcp to format code"
 
 # shellcheck disable=SC2046
-ghcp commit -r "$GITHUB_REPOSITORY" -b "$branch" \
+ghcp commit \
+	-r "$GITHUB_REPOSITORY" \
+	-b "${GITHUB_HEAD_REF:-$GITHUB_REF_NAME}" \
 	-m "$INPUT_COMMIT_MESSAGE" \
 	-C "$ROOT_DIR" $(cat "$tempfile")
 rm "$tempfile"
