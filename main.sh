408441598929f1c7a1ac70030d6e05d4078b303d
Shunsuke Suzuki <suzuki-shunsuke@users.noreply.github.com>
2024-01-27 16:24:00 +0900

    fix: fix a branch on push event (#593)
    * fix: fix a branch on push event
    
    * style: shfmt
    
    * fix: fix for push tag event
    
    ---------
    
    Co-authored-by: suzuki-shunsuke-app[bot] <91834585+suzuki-shunsuke-app[bot]@users.noreply.github.com>

--- main.sh
+++ main.sh
@@ -16,15 +16,21 @@ if [ "$SKIP_PUSH" = true ]; then
 	exit 1
 fi
 
-echo "::notice :: Pushing a commit using ghcp to format code"
+branch=${GITHUB_HEAD_REF:-$GITHUB_REF}
 
-if ! ghcp -v; then
-	echo "::error :: ghcp is required. Please install https://github.com/int128/ghcp"
+if [[ $branch =~ ^refs/tags/ ]]; then
 	exit 1
 fi
 
+if ! ghcp -v; then
+	echo "::error :: ghcp is required to push a commit. Please install https://github.com/int128/ghcp"
+	exit 1
+fi
+
+echo "::notice :: Pushing a commit using ghcp to format code"
+
 # shellcheck disable=SC2046
-ghcp commit -r "$GITHUB_REPOSITORY" -b "$GITHUB_HEAD_REF" \
+ghcp commit -r "$GITHUB_REPOSITORY" -b "$branch" \
 	-m "$INPUT_COMMIT_MESSAGE" \
 	-C "$ROOT_DIR" $(cat "$tempfile")
 rm "$tempfile"
